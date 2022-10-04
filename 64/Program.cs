ae1350c43822a60899ec32c87a6f5ad60083a0a7
May_Day_16 <v.s.oputin@gmail.com6>
2022-10-04 22:54:51 +0500

    64

---
+++ 64/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+Console.Write("Введите целое число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите целое число N: ");
+int N = int.Parse(Console.ReadLine());
+if (M < N) Rec(M, N);
+else RecInvers(M, N);
+
+void Rec(int M, int N)
+{
+    if (M > N)
+    {
+        return;
+    }
+    Console.Write(M + " ");
+    Rec(M + 1, N);
+}
+void RecInvers(int M, int N)
+{
+    if (M < N)
+    {
+        return;
+    }
+    Console.Write(M + " ");
+    RecInvers(M - 1, N);
+}
